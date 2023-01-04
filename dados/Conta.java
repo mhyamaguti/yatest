aa4ea429ba14ec0e8d12ecd404f4de85b9f9707b
Marcelo H. Yamaguti <yamaguti@pucrs.br>
2023-01-04 18:14:22 -0300

    Atributo saldo incluido

--- dados/Conta.java
+++ dados/Conta.java
@@ -2,12 +2,18 @@ package dados;
 
 public class Conta {
     private int numero;
+    private double saldo;
 
     public Conta(int numero) {
         this.numero = numero;
+        this.saldo = 0;
     }
 
     public int getNumero() {
         return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
